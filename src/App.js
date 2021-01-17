a74a66e52df12d4314187c81221a944bf72b0fbb
juniorh28 <juniorh2897@gmail.com>
2021-01-16 19:57:52 -0500

    recieve the api and append the info into a variable to be used

--- src/App.js
+++ src/App.js
@@ -14,6 +14,10 @@ class App extends Component {
         userName: "Billy Bob",
         memberSince: "01/01/2021",
       },
+      debitInfo: [], //will hold an array of objects that holds user's id, amount, description, and date
+      debitAmount: 0, //the debitAmount is the
+      creditInfo: [], //will hold an array of objects that holds user's id, amount, description, and date
+      creditAmount: 0,
     };
   }
 
@@ -22,6 +26,31 @@ class App extends Component {
     newUser.userName = logInInfo.userName;
     this.setState({ currentUser: newUser });
   };
+
+  async componentDidMount() {
+    //more on fetch =>  https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+    try {
+      //Url will grab the information from the debit and credit api and assign it to a varible.
+      //at this point it is not readable.
+      let debitUrl = await fetch("https://moj-api.herokuapp.com/debits");
+      let creditUrl = await fetch("https://moj-api.herokuapp.com/credits");
+      console.log("debitUrl\n");
+      console.log(debitUrl);
+      console.log("creditUrl\n");
+      console.log(creditUrl);
+      this.setState({
+        //once recieved, .json allow you to make the data in the url readable.
+        debitInfo: await debitUrl.json(),
+        creditInfo: await creditUrl.json(),
+      });
+      console.log("debitInfo\n");
+      console.log(this.state.debitInfo);
+      console.log("creditInfo\n");
+      console.log(this.state.creditInfo);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   render() {
     const HomeComponent = () => (
@@ -43,9 +72,19 @@ class App extends Component {
       />
     );
 
-    const CreditsComponent = () => <Credits></Credits>;
+    const CreditsComponent = () => (
+      <Credits
+        creditInfo={this.state.creditInfo}
+        creditAmount={this.state.creditAmount}
+      ></Credits>
+    );
 
-    const DebitsComponent = () => <Debits></Debits>;
+    const DebitsComponent = () => (
+      <Debits
+        debitInfo={this.state.debitInfo}
+        debitAmount={this.state.debitAmount}
+      ></Debits>
+    );
 
     return (
       <Router>
--- src/components/Credits.jsx
+++ src/components/Credits.jsx
@@ -1,28 +1,9 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import Axios from "axios";
-
 class Credits extends Component {
-  handleChange(e) {
-    let cityname = e.target.value;
-    let cityInput = cityname.toUpperCase();
-
-    const url = `http://ctp-zip-api.herokuapp.com/city/${cityInput}`;
-    axios
-      .get(url)
-      .then((res) => {
-        const response = res.data;
-        let zipArr = [];
-        for (const i in response) {
-          zipArr.push(response[i]);
-        }
-
-        this.setState({
-          zipcode: zipArr,
-        });
-      })
-      .catch(console.error());
-  }
+  state = {
+    user: [],
+  };
 
   render() {
     return (
